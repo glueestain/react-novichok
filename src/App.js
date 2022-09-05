6dad7546b20f0fa8bd2d25ca3d9e0d4f7aa11739
glueestain <iznogud4357@yandex.ru>
2022-09-05 22:46:55 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import {BrowserRouter, Link, Route, Routes,Navigate} from "react-router-dom";
+
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
+import AppRouter from "./components/UI/AppRouter";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+            <BrowserRouter>
+                <Navbar/>
+            <AppRouter/>
+            </BrowserRouter>
+  )
 }
 
 export default App;
